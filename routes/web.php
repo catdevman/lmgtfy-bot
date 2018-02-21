1cf429568280a7b950670e63aa74b652517055f5
Lucas Pearson <lpearson@five-startech.com>
2018-02-21 08:57:37 -0500

    lmgtfybot working

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/', function (\Illuminate\Http\Request $request) use ($router) {
+    $url = "http://lmgtfy.com/?q=";
+    $event = $request->json()->all();
+    if($event["type"] === "ADDED_TO_SPACE" && $event['space']['type'] == 'ROOM'){
+        return ["text" => "Thanks for adding me!"];
+    } else if( $event["type"] === "MESSAGE"){
+        $query = urlencode(trim(str_replace("@lmgtfybot", "", $event['message']['text'])));
+        $response = [];
+        $response['sender']['displayName'] = "lmgtfybot";
+        $response['sender']['avatarUrl'] = "https://goo.gl/aeDtrS";
+        $response['text'] = $url . $query;
+        return $response;
+    } else if( $event["type"] === "REMOVE_FROM_SPACE" && $event['space']['type'] == 'ROOM' ){
+        return ["text" => "Later punks I am outta here!"];
+    }
+});
